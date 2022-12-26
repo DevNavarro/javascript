2f52452d2f4d352fa42cce2dba646ae7ea5d59b7
Pedro Navarro <119688953+DevNavarro@users.noreply.github.com>
2022-12-26 15:00:02 -0300

    Base de arquivos para o desafio

--- exercícios/aula012/ex012.js
+++ exercícios/aula012/ex012.js
@@ -1,9 +1,10 @@
-var hora = 14
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatatamente ${hora} horas.`)
 if (hora < 12) {
-    console.log('Bom dia')
+    console.log('Bom dia!')
 } else if (hora < 18) {
-    console.log('Boa tarde')
+    console.log('Boa tarde!')
 } else {
-    console.log('Boa noite')
+    console.log('Boa noite!')
 }
