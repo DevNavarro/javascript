dcff7f5a744ebf59fc9157b5bfe598540a910f97
Pedro Navarro <119688953+DevNavarro@users.noreply.github.com>
2022-12-26 17:00:38 -0300

    aula15 finalizada

--- Desafios/aula14/script.js
+++ Desafios/aula14/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = 'foto-manha.png'
+        document.body.style.background = '#fed673'
+    } else if (hora >=12 && hora < 18) {
+        // Boa tarde!
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#ff9669'
+    } else {
+        // Boa noite!
+        img.src = 'foto-noite.png'
+        document.body.style.background = '#16201f'
+    }
+}
