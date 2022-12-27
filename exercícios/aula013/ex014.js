57d8e982e90089f38a4ceb63fe14839fa31eb103
Pedro Navarro <119688953+DevNavarro@users.noreply.github.com>
2022-12-27 09:53:06 -0300

    Update desafios

---
+++ exercícios/aula013/ex014.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+} while (c <= 0)
+
+
+
+
+/*var c = 1
+while (c <= 600) {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+} */
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
+
