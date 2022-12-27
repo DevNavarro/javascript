a390c735013aa73b183c119b5f9915a533a21c64
Pedro Navarro <119688953+DevNavarro@users.noreply.github.com>
2022-12-27 19:01:07 -0300

    update ex finalizado

--- Desafios/pt4/ex01/script.js
+++ Desafios/pt4/ex01/script.js
@@ -1,14 +1,30 @@
-function contar() {
-    var ini = window.document.querySelector('input#txtinicio')
-    var fi = window.document.querySelector('input#txtfim')
-    var pas = window.document.querySelector('input#txtpasso')
-    var res = window.document.querySelector('div#res')
-    var inicio = Number('ini.value')
-    var fim = Number('fi.value')
-    var passo = Number('pas.value')
+function contar () {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-
-    for (var inicio; index < array.length; index++) {
-        
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[Erro] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo(0) INVÁLIDO, considerando Passo = 1')
+            p = 1
+        }
+        if (i < f) { // CONTAGEM CRESCENTE
+            for(let c = i; c <=f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            } else { // CONTAGEM REGRESSIVA
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
+    
 }
