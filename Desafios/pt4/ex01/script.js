316805ff1e3d562475614a7b4a860867247a09a7
Pedro Navarro <119688953+DevNavarro@users.noreply.github.com>
2022-12-27 18:15:59 -0300

    update ex01

---
+++ Desafios/pt4/ex01/script.js
@@ -0,0 +1,14 @@
+function contar() {
+    var ini = window.document.querySelector('input#txtinicio')
+    var fi = window.document.querySelector('input#txtfim')
+    var pas = window.document.querySelector('input#txtpasso')
+    var res = window.document.querySelector('div#res')
+    var inicio = Number('ini.value')
+    var fim = Number('fi.value')
+    var passo = Number('pas.value')
+
+
+    for (var inicio; index < array.length; index++) {
+        
+    }
+}
