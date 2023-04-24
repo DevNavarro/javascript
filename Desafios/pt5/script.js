f7f591d767cb5781d7187a99c46297ce17014d68
Pedro Navarro <119688953+DevNavarro@users.noreply.github.com>
2023-04-24 11:41:28 -0300

    2.1

--- Desafios/pt5/script.js
+++ Desafios/pt5/script.js
@@ -1,9 +1,61 @@
+let num = window.document.querySelector('input#fnum')
+let lista = window.document.querySelector('select#tab')
+let res = window.document.querySelector('div#res')
+let valores = []
+
+
+function isNumero(n) {
+    if(Number(n) >=1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+function isLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 function adicionar() {
-    let txtn = window.document.getElementById('txtn')
-    let num = [Number(txtn.value)]
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function adicionar() {
+    let fnum = window.document.getElementById('txtn')
+    let num = [Number(fnum.value)]
     let tab = window.document.getElementById('tab')
 
-    if (num < 1 || num > 100 || txtn.value.length == 0) {
+    if (num < 1 || num > 100 || fnum.value.length == 0) {
         window.alert('Verifique o valor Digitado')
     } else {
         let item = window.document.createElement('option')
@@ -15,4 +67,4 @@ function finalizar () {
     window.createElement('div')
     let numcad = window.document.createElement('p')
     numcad.text = `Ao todo, temos ${num.length} números cadastrados`
-}
+} */
